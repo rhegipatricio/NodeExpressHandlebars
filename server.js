0cbca4f1caa09d6620eb670efd10fe1b9bfd25fa
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-06 15:12:15 -0400

    added more for server.js

--- server.js
+++ server.js
@@ -4,4 +4,17 @@ var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var mysql = require("mysql");
 
+var app = express();
+app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false}));
+
+var exphbs = require('express-handlebars');
+app.engine('handlerbars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlerbars');
+
+var router = require('./controllers/controller.js');
+
 var PORT = process.env.PORT || 3300;
+app.listen(PORT);
+console.log('Listening on ' + PORT);
