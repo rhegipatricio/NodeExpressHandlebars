21a3711275580aa47f5bdb62abddef076a0fc42b
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-07 03:51:17 -0400

    added skeleton html

--- server.js
+++ server.js
@@ -1,36 +1,21 @@
 var express = require('express');
-var methodOverride = require('method-override');
+var path = require ("path");
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var mysql = require("mysql");
-var path = require ("path");
+var controller = require('./controllers/controller.js');
+
 
 var app = express();
 var PORT = process.env.PORT || 3300;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', controller);
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
-var exphbs = require('express-handlebars');
 app.engine('handlerbars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var router = require('./controllers/controller.js');
-
-var connection = mysql.createConnection({
-	host	: 'localhost',
-	user	: 'root',
-	password: '',
-	database: '' 
-});
-
-connection.connect(function(error) {
-	if(error){
-		throw error;
-	}
-	console.log('Connected to MySQL server as ID = ', connection.threadId);
-	app.listen(PORT, function() {
+app.listen(PORT, function() {
 		console.log("App listening on PORT " + PORT);
-	});
 });
