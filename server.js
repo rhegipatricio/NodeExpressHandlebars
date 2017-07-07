dff20ddd83d1afb812fc18d1bb7d2317320c8218
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-06 20:37:57 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -3,18 +3,34 @@ var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var mysql = require("mysql");
+var path = require ("path");
 
 var app = express();
-app.use(express.static(process.cwd() + '/public'));
-app.use(express.static('public'));
+var PORT = process.env.PORT || 3300;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: false}));
 
 var exphbs = require('express-handlebars');
 app.engine('handlerbars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlerbars');
+app.set('view engine', 'handlebars');
 
 var router = require('./controllers/controller.js');
 
-var PORT = process.env.PORT || 3300;
-app.listen(PORT);
-console.log('Listening on ' + PORT);
+var connection = mysql.createConnection({
+	host	: 'localhost',
+	user	: 'root',
+	password: '',
+	database: '' 
+});
+
+connection.connect(function(error) {
+	if(error){
+		throw error;
+	}
+	console.log('Connected to MySQL server as ID = ', connection.threadId);
+	app.listen(PORT, function() {
+		console.log("App listening on PORT " + PORT);
+	});
+});
