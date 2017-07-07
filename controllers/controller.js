3b263496150b9e850ef6a30b05bab78402e3347c
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-07 08:12:01 -0400

    changed controller for functionality

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,2 +1,23 @@
-var express = require('express');
-var model = require("/models/model.js");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "root:",
+	password: "",
+	database: "nba"
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as ID " + connection.threadID);
+});
+
+
+
+
+
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,21 +1,25 @@
+//npm dependencies
 var express = require('express');
 var path = require ("path");
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var controller = require('./controllers/controller.js');
 
+var controller = require('./controllers/controller.js');
+app.use('/', controller);
 
 var app = express();
-var PORT = process.env.PORT || 3300;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', controller);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.engine('handlerbars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//server starter
+var PORT = process.env.PORT || 3300;
 app.listen(PORT, function() {
 		console.log("App listening on PORT " + PORT);
 });
